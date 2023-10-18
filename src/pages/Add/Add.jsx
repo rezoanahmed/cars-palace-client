794095cadf061d81abaea0d60b0bd37e184a4be7
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-18 16:56:45 +0600

    car details files are connected to database

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,10 +1,34 @@
+import Swal from "sweetalert2";
 
 const Add = () => {
     const handleSubmit = e =>{
         e.preventDefault();
         const form = e.target;
-        const type=form.type.value;
-        console.log(type);
+        const brand = form.brand.value;
+        const model = form.model.value;
+        const year = form.year.value;
+        const type = form.type.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const details = form.details.value;
+
+        const car = {brand, model, year, type, price, rating, details};
+        // console.log(car);
+
+        fetch("http://localhost:5000/cars",{
+            method: "POST",
+            headers:{
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(car),
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire("Great!!!", "Car Has Been Added Successfully", "success");
+            }
+        })
     }
     return (
         <div className="p-4">
@@ -13,13 +37,13 @@ const Add = () => {
                 <form onSubmit={handleSubmit} className="flex gap-2 flex-col justify-center mt-10">
                     {/* brand and model */}
                     <div className="flex gap-2">
-                        <input type="text" placeholder="Brand" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
-                        <input type="text" placeholder="Model" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
+                        <input name='brand' type="text" placeholder="Brand" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
+                        <input name='model' type="text" placeholder="Model" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
                     </div>
                     {/* Type and Model Year */}
                     <div className="flex gap-2">
                         {/* <input type="text" placeholder="Type" className="input input-bordered input-info border-blue-600 w-full max-w-xs" /> */}
-                        <input type="text" placeholder="Model Year" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
+                        <input name='year' type="text" placeholder="Model Year" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
                         <select name='type' className="select select-bordered border-blue-600 w-full ">
                             <option disabled selected>Select Type</option>
                             <option>Sedan</option>
@@ -28,18 +52,20 @@ const Add = () => {
                             <option>Sports</option>
                             <option>Coupe</option>
                             <option>Convertible</option>
+                            <option>Electric</option>
                             <option>MPV</option>
+                            <option>Others</option>
                         </select>
                         
                     </div>
                     {/* Price and Rating */}
-                    <div className="flex gap-2">
-                        <input type="text" placeholder="Price" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
-                        <input type="text" placeholder="Rating" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
+                    <div name='price' className="flex gap-2">
+                        <input name='price' type="text" placeholder="Price" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
+                        <input name='rating' type="text" placeholder="Rating" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
                     </div>
                     {/* Description */}
                     <div className="flex gap-2">
-                        <textarea type="text" placeholder="Write Short Description" className="p-4 h-24 input input-bordered input-info border-blue-600 w-full" />
+                        <textarea name='details' type="text" placeholder="Write Short Description" className="p-4 h-24 input input-bordered input-info border-blue-600 w-full" />
 
                     </div>
                     <input type="submit" value="Add Car" className="bg-blue-600 text-white px-2 py-2 rounded-md" />
