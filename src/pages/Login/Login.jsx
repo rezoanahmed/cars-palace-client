9995592741d6fec650745e37619e57167d75e878
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-18 09:54:32 +0600

    login page has been created

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,15 @@
+import { NavLink } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-            <h1>hello</h1>
-            <div>
-                <form>
-                <input type="text" placeholder="Type here" className="input input-bordered input-info w-full max-w-xs" />
+        <div className="p-4">
+            <h1 className="text-center text-3xl">Login Now!</h1>
+            <div className="flex justify-center">
+                <form className="flex gap-2 flex-col justify-center mt-10">
+                    <input type="text" placeholder="Enter Your Email" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
+                    <input type="text" placeholder="Enter Your Password" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
+                    <input type="submit" value="Login" className="bg-blue-600 text-white px-2 py-2 rounded-md" />
+                    <p>Don't Have Account Yet? <NavLink to='/register' className={`text-blue-600 font-medium`}>Register</NavLink></p>
                 </form>
             </div>
         </div>
