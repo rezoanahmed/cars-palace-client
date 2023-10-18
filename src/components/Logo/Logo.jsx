695fd7a4a22dec77de71195d13a444f080ff208f
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-18 09:11:11 +0600

    initialized the project

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,12 @@
+
+
+const Logo = () => {
+    return (
+        <div>
+            <img src={"https://i.ibb.co/VJppQx6/cars-palace.png"} alt="" />            
+            <h1>Cars <span className="text-blue-600">Palace</span></h1>
+        </div>
+    );
+};
+
+export default Logo;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+import Homepage from "../pages/Homepage/Homepage";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Homepage></Homepage>,
+    }
+])
+
