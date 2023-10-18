c2ddb9ba8979af64b69a81cac94d034e3e6960b3
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-18 12:29:29 +0600

    little styles changed in login and register form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,39 +6,39 @@ import Swal from "sweetalert2";
 
 const Login = () => {
     const context = useContext(AuthContext);
-    const {login, googleSignIn } = context;
-    const handleLogin = e =>{
+    const { login, googleSignIn } = context;
+    const handleLogin = e => {
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email,password);
+        console.log(email, password);
         login(email, password)
-        .then(userCredentils=>{
-            console.log(userCredentils);
-            if(userCredentils){
-                Swal.fire("Welcome!", "Login Succeeded", 'success');
-            }
-        })
-        .catch(error=>{
-            console.log(error.code);
-            if(error.code=="auth/invalid-login-credentials"){
-                Swal.fire("Error!", "Email and Password Didn't Match", 'error')
-            }
-        })
+            .then(userCredentils => {
+                console.log(userCredentils);
+                if (userCredentils) {
+                    Swal.fire("Welcome!", "Login Succeeded", 'success');
+                }
+            })
+            .catch(error => {
+                console.log(error.code);
+                if (error.code == "auth/invalid-login-credentials") {
+                    Swal.fire("Error!", "Email and Password Didn't Match", 'error')
+                }
+            })
     }
 
     return (
         <div className="p-4">
             <h1 className="text-center text-3xl">Login Now!</h1>
-            <div className="flex justify-center">
+            <div className="flex flex-col items-center justify-center">
                 <form onSubmit={handleLogin} className="flex gap-2 flex-col justify-center mt-10">
                     <input name='email' type="text" placeholder="Enter Your Email" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
                     <input name='password' type="text" placeholder="Enter Your Password" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
                     <input type="submit" value="Login" className="bg-blue-600 text-white px-2 py-2 rounded-md" />
-                    <button onClick={googleSignIn} className="flex items-center justify-center gap-2 border border-blue-600 px-4 py-2 rounded-lg hover:bg-blue-50 w-full"><FcGoogle className="text-xl"></FcGoogle>Sign In with Google</button>
-                    <p>Don't Have Account Yet? <NavLink to='/register' className={`text-blue-600 font-medium`}>Register</NavLink></p>
                 </form>
+                <button onClick={googleSignIn} className="flex items-center justify-center gap-2 border border-blue-600 px-4 py-2 rounded-lg hover:bg-blue-50 mt-2"><FcGoogle className="text-xl"></FcGoogle>Continue with Google</button>
+                <p className="mt-2">Don't have account yet? <NavLink to='/register' className={`text-blue-600 font-medium`}>Register</NavLink></p>
             </div>
         </div>
     );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -6,11 +6,11 @@ import Swal from "sweetalert2";
 
 const Register = () => {
     const context = useContext(AuthContext);
-    const { register, googleSignIn} = context;
+    const { register, googleSignIn } = context;
     // console.log(test);
 
 
-    const handleResister = e =>{
+    const handleResister = e => {
         e.preventDefault();
         const form = e.target;
         const name = form.name.value;
@@ -20,19 +20,19 @@ const Register = () => {
 
         // console.log(name, email, password, photo);
         register(email, password)
-        .then(userCredentials=>{
-            console.log(userCredentials.user.email);
-            if(userCredentials.user.email){
-               Swal.fire("Great!", "Successfully Registered To Cars Palace", "success"); 
-            }
-        })
-        .catch(err=>console.log(err))
+            .then(userCredentials => {
+                console.log(userCredentials.user.email);
+                if (userCredentials.user.email) {
+                    Swal.fire("Great!", "Successfully Registered To Cars Palace", "success");
+                }
+            })
+            .catch(err => console.log(err))
     }
 
     return (
         <div className="p-4">
             <h1 className="text-center text-3xl">Register Today and Explore Our Exotic Cars!</h1>
-            <div className="flex justify-center">
+            <div className="flex flex-col items-center justify-center">
                 <form onSubmit={handleResister} className="flex gap-2 flex-col justify-center mt-10">
                     <input name='name' type="text" placeholder="Enter Your Full Name" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
                     <input name='email' type="text" placeholder="Enter Your Email" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
@@ -40,8 +40,8 @@ const Register = () => {
                     <input name='photo' type="text" placeholder="Enter Your Photo URL" className="input input-bordered input-info border-blue-600 w-full max-w-xs" />
                     <input type="submit" value="Register" className="bg-blue-600 text-white px-2 py-2 rounded-md" />
                 </form>
-                    <button onClick={googleSignIn} className="flex items-center justify-center gap-2 border border-blue-600 px-4 py-2 rounded-lg hover:bg-blue-50 w-full"><FcGoogle className="text-xl"></FcGoogle>Continue with Google</button>
-                    <p>Already Have Account? <NavLink to='/login' className={`text-blue-600 font-medium`}>Login</NavLink></p>
+                <button onClick={googleSignIn} className="flex items-center justify-center gap-2 border border-blue-600 px-4 py-2 rounded-lg hover:bg-blue-50 mt-2"><FcGoogle className="text-xl"></FcGoogle>Continue with Google</button>
+                <p className="mt-2">Already Have Account? <NavLink to='/login' className={`text-blue-600 font-medium`}>Login</NavLink></p>
             </div>
         </div>
     );
