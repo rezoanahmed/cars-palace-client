b8e2347228c044ea33d1529e4f8170b402db7ddc
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-10-19 16:32:25 +0600

    added delete functionality in cart

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,48 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Cart = () => {
+
     const data = useLoaderData();
+    const [cars,setCars]=useState(data);
+
+    const handleDelete = (id) => {
+        // console.log("deleting", id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/cart/${id}`, {
+                    method: "DELETE",
+                })
+                    .then(res => res.json())
+                    .then(data => {
+
+                        if (data.deletedCount>0) {
+                            const remaining = cars.filter(car=>car._id!=id);
+                            setCars(remaining);
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                                )
+                            }
+                            
+                    })
+
+            }
+        })
+    }
     return (
         <div className='flex h-[75vh] justify-center items-center'>
-            {data.length > 0 ?
+            {cars?.length > 0 ?
                 <>
                     <div className="overflow-x-auto">
                         <table className="table">
@@ -15,15 +53,17 @@ const Cart = () => {
                                     <th>Model</th>
                                     <th>Type</th>
                                     <th>Price</th>
+                                    <th>Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {data.map((element, index) => <>
+                                {cars?.map((element, index) => <>
                                     <tr className="hover">
                                         <th>{index + 1}</th>
                                         <td>{element.brand} {element.model} {element.year}</td>
                                         <td>{element.type}</td>
                                         <td>${element.price}</td>
+                                        <td className=""><button onClick={() => handleDelete(element._id)} className="btn font-bold text-red-700">X</button></td>
                                     </tr>
                                 </>)}
                             </tbody>
